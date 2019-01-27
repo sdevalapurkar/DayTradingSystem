80498e2644e8f57ad708fdfac7183569bacb72bd
Caity Gossland <caitygossland@gmail.com>
2019-01-27 15:28:50 -0800

    Updates to posts and gets

--- web-server/server.js
+++ web-server/server.js
@@ -17,86 +17,86 @@ app.put('/api/ADD', function(req, res) {
   res.send('hey from put')
 });
 
-app.put('/api/BUY', function(req, res) {
+app.post('/api/BUY', function(req, res) {
   console.log(req.body);
   console.log('Buy endpoint');
   res.send('hey from put')
 });
 
-app.put('/api/COMMIT_BUY', function(req, res) {
+app.post('/api/COMMIT_BUY', function(req, res) {
   console.log(req.body);
   console.log('Commit Buy endpoint');
   res.send('hey from put')
 });
 
-app.put('/api/CANCEL_BUY', function(req, res) {
+app.post('/api/CANCEL_BUY', function(req, res) {
   console.log(req.body);
   console.log('Cancel buy endpoint');
   res.send('hey from put')
 });
 
-app.put('/api/SELL', function(req, res) {
+app.post('/api/SELL', function(req, res) {
   console.log(req.body);
   console.log('Sell endpoint');
   res.send('hey from put')
 });
 
-app.put('/api/COMMIT_SELL', function(req, res) {
+app.post('/api/COMMIT_SELL', function(req, res) {
   console.log(req.body);
   console.log('Commit sell endpoint');
   res.send('hey from put')
 });
 
-app.put('/api/CANCEL_SELL', function(req, res) {
+app.post('/api/CANCEL_SELL', function(req, res) {
   console.log(req.body);
   console.log('Cancel sell endpoint');
   res.send('hey from put')
 });
 
-app.put('/api/SET_BUY_AMOUNT', function(req, res) {
+app.post('/api/SET_BUY_AMOUNT', function(req, res) {
   console.log(req.body);
   console.log('Set buy amount endpoint');
   res.send('hey from put')
 });
 
-app.put('/api/CANCEL_SET_BUY', function(req, res) {
+app.post('/api/CANCEL_SET_BUY', function(req, res) {
   console.log(req.body);
   console.log('Cancel set buy endpoint');
   res.send('hey from put')
 });
 
-app.put('/api/SET_BUY_TRIGGER', function(req, res) {
+app.post('/api/SET_BUY_TRIGGER', function(req, res) {
   console.log(req.body);
   console.log('Set sell trigger endpoint');
   res.send('hey from put')
 });
 
-app.put('/api/SET_SELL_AMOUNT', function(req, res) {
+app.post('/api/SET_SELL_AMOUNT', function(req, res) {
   console.log(req.body);
   console.log('Set sell amount endpoint');
   res.send('hey from put')
 });
 
-app.put('/api/SET_SELL_TRIGGER', function(req, res) {
+app.post('/api/SET_SELL_TRIGGER', function(req, res) {
   console.log(req.body);
   console.log('Set sell trigger endpoint');
   res.send('hey from put')
 });
 
-app.put('/api/CANCEL_SET_SELL', function(req, res) {
+app.post('/api/CANCEL_SET_SELL', function(req, res) {
   console.log(req.body);
   console.log('Cancel set sell endpoint');
   res.send('hey from put')
 });
 
-app.put('/api/DUMPLOG', function(req, res) {
-  console.log(req.body);
+app.get('/api/DUMPLOG', function(req, res) {
+  console.log(req.query);
   console.log('Dumplog endpoint');
   res.send('hey from put')
 });
 
-app.put('/api/DISPLAY_SUMMARY', function(req, res) {
-  console.log(req.body);
+app.get('/api/DISPLAY_SUMMARY', function(req, res) {
+  console.log(req.query);
   console.log('Display summary endpoint');
   res.send('hey from put')
 });
--- workload-generator/generator.py
+++ workload-generator/generator.py
@@ -8,20 +8,28 @@ for line in lines:
   print(line)
   commands = [command.strip() for command in line.split(',')]
   command_type = commands.pop(0)
-  print(commands)
 
-  # only GET
+  # GET
   if command_type == 'QUOTE':
     r = requests.get('http://localhost:8009/api/{}?user_id={}&stock_symbol={}'.format(command_type, commands[0], commands[1]))
+  elif command_type == 'DISPLAY_SUMMARY':
+    r = requests.get('http://localhost:8009/api/{}?user_id={}'.format(command_type, commands[0]))
+  elif command_type == 'DUMPLOG' and len(commands) == 2:
+    r = requests.get('http://localhost:8009/api/{}?user_id={}&filename={}'.format(command_type, commands[0], commands[1]))
+  elif command_type == 'DUMPLOG' and len(commands) == 1:
+    r = requests.get('http://localhost:8009/api/{}?filename={}'.format(command_type, commands[0]))
 
-  # rest are POST
+  # PUT
+  elif command_type == 'ADD':
+    command_dict = {
+      'user_id': commands[0],
+      'amount': commands[1]
+    }
+    r = requests.put('http://localhost:8009/api/{}'.format(command_type), data=command_dict)
+
+  # POST
   else:
-    if command_type == 'ADD':
-      command_dict = {
-        'user_id': commands[0],
-        'amount': commands[1]
-      }
-    elif command_type in ('BUY', 'SELL', 'SET_BUY_AMOUNT', 'SET_BUY_TRIGGER', 'SET_SELL_AMOUNT', 'SET_SELL_TRIGGER'):
+    if command_type in ('BUY', 'SELL', 'SET_BUY_AMOUNT', 'SET_BUY_TRIGGER', 'SET_SELL_AMOUNT', 'SET_SELL_TRIGGER'):
       command_dict = {
         'user_id': commands[0],
         'stock_symbol': commands[1],
@@ -32,21 +40,9 @@ for line in lines:
         'user_id': commands[0],
         'stock_symbol': commands[1]
       }
-    elif command_type in ('COMMIT_BUY', 'CANCEL_BUY', 'COMMIT_SELL', 'CANCEL_SELL', 'DISPLAY_SUMMARY'):
+    elif command_type in ('COMMIT_BUY', 'CANCEL_BUY', 'COMMIT_SELL', 'CANCEL_SELL'):
       command_dict = {
         'user_id': commands[0],
       }
-    elif command_type == 'DUMPLOG':
-      if len(commands) == 2:
-          command_dict = {
-            'user_id': commands[0],
-            'filename': commands[1]
-          }
-      else:
-          command_dict = {
-            'filename': commands[0]
-          }
-
-    print(command_type, command_dict)
-    r = requests.put('http://localhost:8009/api/{}'.format(command_type), data=command_dict)
+    r = requests.post('http://localhost:8009/api/{}'.format(command_type), data=command_dict)
 
