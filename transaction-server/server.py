11f23066a985045f50d89d5dee0c7dbfd59b000f
LeeZeitz <andrew.l.zeitz@gmail.com>
2019-01-27 15:10:20 -0800

    debug and finalize buy endpoint in python

--- transaction-server/server.py
+++ transaction-server/server.py
@@ -1,70 +1,95 @@
-from flask import Flast, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 from crate import client
 import json
 
 c = ''
 
-@app.route('/add')
+# Add funds to account
+@app.route('/add', methods=['GET', 'POST'])
 def addHandler():
+    # Check http method is a post
     if request.method == 'POST':
+        # Parse posted data into json object
         data = request.get_json()
-        query_string = 'UPDATE users SET balance = balance + {} where user_id = {}'
-        c.execute(query_string.format(data['amount'], data['user_id']))
+        # Insert new user if they don't already exist, otherwise update their balance
+        query_string = 'INSERT INTO users (user_id, balance) VALUES ({1}, {0}) ON CONFLICT (user_id) DO UPDATE SET balance = balance + {0};'
+
+        try:
+            c.execute(query_string.format(data['amount'], data['user_id']))
+        except:
+            return 404
 
         response = jsonify(success=True)
         response.status_code = 200
+        return response
     else:
         return 404
 
 
 @app.route('/quote')
-def quateHandler()
+def quoteHandler():
+    pass
 
 @app.route('/buy')
-def buyHandler()
+def buyHandler():
+    pass
 
 @app.route('/commit_buy')
-def commitBuyHandler()
+def commitBuyHandler():
+    pass
 
 @app.route('/cancel_buy')
-def cancelBuyHandler()
+def cancelBuyHandler():
+    pass
 
 @app.route('/sell')
-def sellHandler()
+def sellHandler():
+    pass
 
 @app.route('/commit_sell')
-def commitSellHandler()
+def commitSellHandler():
+    pass
 
 @app.route('/cancel_sell')
-def cancelSellHandler()
+def cancelSellHandler():
+    pass
 
 @app.route('/set_buy_amount')
-def setBuyAmountHandler()
+def setBuyAmountHandler():
+    pass
 
 @app.route('/cancel_set_buy')
-def cancelSetBuyHandler()
+def cancelSetBuyHandler():
+    pass
 
 @app.route('/set_buy_trigger')
-def setBuyTriggerHandler()
+def setBuyTriggerHandler():
+    pass
 
 @app.route('/set_sell_amount')
-def setSellAmountHandler()
+def setSellAmountHandler():
+    pass
 
 @app.route('/set_sell_trigger')
-def setSellTriggerHandler()
+def setSellTriggerHandler():
+    pass
 
 @app.route('/cancel_set_sell')
-def cancelSetSellHandler()
+def cancelSetSellHandler():
+    pass
 
 @app.route('/dumplog')
-def dumpLogHandler()
+def dumpLogHandler():
+    pass
 
 @app.route('/display_summary')
-def displaySummaryHandler()
+def displaySummaryHandler():
+    pass
 
 if __name__ == '__main__':
-    app.run()
     connection = client.connect("localhost:4200", username="crate")
     c = connection.cursor()
+
+    app.run(port=3000, debug=True)
 
