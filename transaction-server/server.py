09768e4235a1cf6fc685066916d951df668ba348
LeeZeitz <andrew.l.zeitz@gmail.com>
2019-01-27 12:16:51 -0800

    connect to database, implement add money function

--- transaction-server/server.py
+++ transaction-server/server.py
@@ -1,10 +1,22 @@
-from flask import Flast
+from flask import Flast, jsonify
 app = Flask(__name__)
+from crate import client
+import json
 
-
+c = ''
 
 @app.route('/add')
-def addHandler()
+def addHandler():
+    if request.method == 'POST':
+        data = request.get_json()
+        query_string = 'UPDATE users SET balance = balance + {} where user_id = {}'
+        c.execute(query_string.format(data['amount'], data['user_id']))
+
+        response = jsonify(success=True)
+        response.status_code = 200
+    else:
+        return 404
+
 
 @app.route('/quote')
 def quateHandler()
@@ -53,3 +65,6 @@ def displaySummaryHandler()
 
 if __name__ == '__main__':
     app.run()
+    connection = client.connect("localhost:4200", username="crate")
+    c = connection.cursor()
+
