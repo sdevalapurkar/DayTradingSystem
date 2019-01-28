0299cc4a48c675bc0baeaa1bbfc62965b4f3359f
LeeZeitz <andrew.l.zeitz@gmail.com>
2019-01-28 15:35:52 -0800

    add sell_amounts and buy_amounts tables to db

--- crate_entry_point.sh
+++ crate_entry_point.sh
@@ -10,4 +10,16 @@ crash -c "CREATE TABLE IF NOT EXISTS stocks(
     quantity INT,
     PRIMARY KEY (user_id, symbol)
 );"
+crash -c "CREATE TABLE IF NOT EXISTS buy_amounts(
+    user_id STRING,
+    symbol STRING,  
+    quantity INT,
+    PRIMARY KEY (user_id, symbol)    
+);"
+crash -c "CREATE TABLE IF NOT EXISTS sell_amounts(
+    user_id STRING,
+    symbol STRING,  
+    quantity INT,
+    PRIMARY KEY (user_id, symbol)
+);"
 exec "$@"
