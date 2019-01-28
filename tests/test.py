5a81a27c4bf10d8580aab3394567a2abbeb74857
LeeZeitz <andrew.l.zeitz@gmail.com>
2019-01-27 22:12:29 -0800

    add a test file for the first three endpoints

--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,7 @@ import requests
 from crate import client
 import time
 
+URL_ROOT = 'http://localhost:8080'
 
 connection = client.connect("localhost:4200", username="crate")
 c = connection.cursor()
@@ -10,11 +11,11 @@ c = connection.cursor()
 def test_add():
 
     data = {
-        'user_id': 69,
-        'amount': 420
+        'userID': '69',
+        'balance': 420
     }
 
-    r = requests.post('http://localhost:3000/add', json=data)
+    r = requests.post(URL_ROOT + '/add', json=data)
 
     time.sleep(2)
 
@@ -24,7 +25,26 @@ def test_add():
     print (c.fetchall())
 
 
+def test_buy():
+    data = {
+        'userID': '69',
+        'amount': 20,
+        'symbol': 'abe'
+    }
+
+    r = requests.post(URL_ROOT + '/buy', json=data)
+
+
+def test_commit_buy():
+    data = {
+        'userID': '69'
+    }
+
+    r = requests.post(URL_ROOT + '/commit_buy', json=data)
+
 
 if __name__ == '__main__':
     
-    test_add()
+    #test_add()
+    #test_buy()
+    test_commit_buy()
