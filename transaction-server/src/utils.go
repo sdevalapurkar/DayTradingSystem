80937cba208ced25fca1861d00db149e0f785389
Graeme Bates <graemebates10@hotmail.com>
2019-02-07 21:50:32 -0500

    lab comp garb

--- transaction-server/src/utils.go
+++ transaction-server/src/utils.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"strconv"
-	"time"
+	"strings"
 
-	"github.com/go-redis/redis"
+	"strconv"
+
+	"net"
+
 	_ "github.com/herenow/go-crate"
 )
 
@@ -37,6 +37,21 @@ func failGracefully(err error, msg string) {
 		fmt.Printf("%s: %s", msg, err)
 	}
 }
+func SocketClient(symbol string, userID string) string {
+	addr := "quoteserve.seng.uvic.ca:4452"
+	conn, err := net.Dial("tcp", addr)
+
+	defer conn.Close()
+
+	failOnError(err, "Failed to connect to quote server")
+
+	payload := fmt.Sprintf("%s,%s\n", symbol, userID)
+	conn.Write([]byte(payload))
+
+	buff := make([]byte, 1024)
+	n, _ := conn.Read(buff)
+	return string(buff[:n])
+}
 
 // Returns a fresh quote for a given stock symbol.
 // If there is a fresh quote cached, then that value is returned. Otherwise, it fetches and stores one.
@@ -46,26 +61,23 @@ func failGracefully(err error, msg string) {
 func getQuote(symbol string, transactionNum int, userID string) float64 {
 	// Check if symbol is in cache
 	quote, err := cache.Get(symbol).Result()
-
-	if err == redis.Nil {
-		// Get quote from the quote server and store it with ttl 60s
-		r, err := http.Get("http://localhost:3000/quote")
-		failOnError(err, "Failed to retrieve quote from quote server")
-		defer r.Body.Close()
-
-		failOnError(err, "Failed to parse quote server response")
-		decoder := json.NewDecoder(r.Body)
-
+	fmt.Println(err)
+	if quote == "" {
+		//Get quote from the quote server and store it with ttl 60s
+		r := SocketClient(symbol, userID)
+		var err error
 		res := struct {
-			CryptoKey string
-			Quote     float64
-		}{"", 0.0}
+			CryptoKey       string
+			Quote           float64
+			QuoteServerTime int64
+		}{"", 0.0, 0}
+		spl := strings.Split(r, ",")
+		res.QuoteServerTime, err = strconv.ParseInt(spl[3], 10, 64)
+		res.CryptoKey = spl[4]
+		res.Quote, err = strconv.ParseFloat(spl[0], 64)
+		failOnError(err, "failed to get stuff from quote")
 
-		err = decoder.Decode(&res)
-		failOnError(err, "Failed to parse quote server response data")
-
-		quoteServerTime := time.Now().UTC().Unix()
-		logQuoteServer(transactionNum, "transaction-server", userID, symbol, res.CryptoKey, quoteServerTime, res.Quote)
+		logQuoteServer(transactionNum, "transaction-server", userID, symbol, res.CryptoKey, res.QuoteServerTime, res.Quote)
 
 		cache.Set(symbol, strconv.FormatFloat(res.Quote, 'f', -1, 64), 60000000000)
 		return res.Quote
