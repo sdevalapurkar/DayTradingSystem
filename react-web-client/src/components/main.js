b8742c8468234e0dcdd68d462e767a8df181e1ce
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2019-03-22 23:38:37 -0700

    add my account page with mock table data

--- react-web-client/src/components/main.js
+++ react-web-client/src/components/main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import TeamMembers from './contact';
 import Trading from './trading';
+import MyAccount from './myaccount';
 import PropTypes from 'prop-types';
 
 class Main extends Component {
@@ -14,10 +15,66 @@ class Main extends Component {
             <Switch>
                 <Route path="/contact" component={TeamMembers} />
                 <Route path="/login" render={(props) => <Trading {...props} userState={this.props.userState} /> } />
+                <Route path="/myaccount" render={(props) => <MyAccount {...props} data = {tableData} /> } />
             </Switch>
         );
     }
 }
+
+var tableData = {
+    columns: ['Service', 'Cost/Unit', 'Unit', 'Units Requested'],
+    rows: [{
+      'Service': 'Veterinary Assitance',
+      'Cost/Unit': 50,
+      'Unit': '1 Hour',
+      'Units Requested': 12
+    }, {
+      'Service': 'Veterinary Assitance',
+      'Cost/Unit': 50,
+      'Unit': '1 Hour',
+      'Units Requested': 12
+    }, {
+      'Service': 'Veterinary Assitance',
+      'Cost/Unit': 50,
+      'Unit': '1 Hour',
+      'Units Requested': 12
+    }, {
+      'Service': 'Veterinary Assitance',
+      'Cost/Unit': 50,
+      'Unit': '1 Hour',
+      'Units Requested': 12
+    }, {
+      'Service': 'Veterinary Assitance',
+      'Cost/Unit': 50,
+      'Unit': '1 Hour',
+      'Units Requested': 12
+    }, {
+      'Service': 'Veterinary Assitance',
+      'Cost/Unit': 50,
+      'Unit': '1 Hour',
+      'Units Requested': 12
+    }, {
+      'Service': 'Veterinary Assitance',
+      'Cost/Unit': 50,
+      'Unit': '1 Hour',
+      'Units Requested': 12
+    }, {
+      'Service': 'Veterinary Assitance',
+      'Cost/Unit': 50,
+      'Unit': '1 Hour',
+      'Units Requested': 12
+    }, {
+      'Service': 'Veterinary Assitance',
+      'Cost/Unit': 50,
+      'Unit': '1 Hour',
+      'Units Requested': 12
+    }, {
+      'Service': 'foo',
+      'Unit': null,
+      'Cost/Unit': undefined,
+      'Units Requested': 42
+    }]
+};
 
 Main.propTypes = {
     userState: PropTypes.any,
