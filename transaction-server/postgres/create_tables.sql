de5bdad3b650855652774936390c5cfb13090057
LeeZeitz <andrew.l.zeitz@gmail.com>
2019-04-01 14:19:31 -0700

    add postgres docker file and entry sql script w/ tables

---
+++ transaction-server/postgres/create_tables.sql
@@ -0,0 +1,33 @@
+CREATE USER trade_user;
+
+CREATE TABLE IF NOT EXISTS users(
+    user_id VARCHAR(64),
+    balance FLOAT,
+    PRIMARY KEY (user_id)
+);
+CREATE TABLE IF NOT EXISTS stocks(
+    user_id VARCHAR(64),
+    symbol VARCHAR(64),
+    quantity INT,
+    PRIMARY KEY (user_id, symbol)
+);
+CREATE TABLE IF NOT EXISTS buy_amounts(
+    user_id VARCHAR(64),
+    symbol VARCHAR(64),  
+    quantity INT,
+    PRIMARY KEY (user_id, symbol)    
+);
+CREATE TABLE IF NOT EXISTS sell_amounts(
+    user_id VARCHAR(64),
+    symbol VARCHAR(64),  
+    quantity INT,
+    PRIMARY KEY (user_id, symbol)
+);
+CREATE TABLE IF NOT EXISTS triggers(
+    user_id VARCHAR(64),
+    symbol VARCHAR(64),
+    price FLOAT,
+    method VARCHAR(64),
+    transaction_num INT,
+    PRIMARY KEY (user_id, symbol, method)
+);
