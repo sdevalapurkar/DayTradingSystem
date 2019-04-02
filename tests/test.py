04c1cf7147bb9bc4610fa63ca1c52d2b9b279670
Caity Gossland <caitygossland@gmail.com>
2019-04-01 21:02:35 -0700

    initial unittest, add works

--- tests/test.py
+++ tests/test.py
@@ -1,187 +1,158 @@
+import unittest
 import requests
 import time
+from crate import client
 
 URL_ROOT = 'http://localhost:8080'
 
+class TestTransactionServer(unittest.TestCase):
 
-# connection = client.connect("192.168.99.100:4200", username="crate")
-# c = connection.cursor()
+    def setUp(self):
+        self.connection = client.connect("http://localhost:4200", username="crate")
+        self.c = self.connection.cursor()
 
-
-def test_add():
-
-    data = {
-        'userID': '69',
-        'balance': 420
-    }
-
-    r = requests.post(URL_ROOT + '/add', json=data)
-
-    # time.sleep(2)
-
-    # connection = client.connect("localhost:4200", username="crate")
-    # c = connection.cursor()
-    # c.execute('SELECT * FROM USERS;')
-    # print (c.fetchall())
+    def test_add(self):
+        data = {
+          'userID': 'User1',
+          'amount': 100.00,
+          'transactionNum': 1
+        }
+        requests.post(URL_ROOT + '/add', json=data)
+        time.sleep(1)
+        self.c.execute('SELECT * FROM USERS;')
+        self.assertEqual(self.c.fetchone(), [100.0, 'User1'])
 
 
-def test_buy():
-    data = {
-        'userID': '69',
-        'amount': 400,
-        'symbol': 'abe'
-    }
-
-    r = requests.post(URL_ROOT + '/buy', json=data)
-
-
-def test_commit_buy():
-    data = {
-        'userID': '69'
-    }
-
-    r = requests.post(URL_ROOT + '/commit_buy', json=data)
-
-def test_cancel_buy():
-    data = {
-        'userID': '69'
-    }
-
-    r = requests.post(URL_ROOT + '/cancel_buy', json=data)
+#   def test_buy(self):
+#       data = {
+#           'userID': '69',
+#           'amount': 400,
+#           'symbol': 'abe'
+#       }
+#       r = requests.post(URL_ROOT + '/buy', json=data)
 
 
-def test_sell():
-    data = {
-        'userID': '69',
-        'amount': 300,
-        'symbol': 'abc'
-    }
-
-    r = requests.post(URL_ROOT + '/sell', json=data)
+#   def test_commit_buy(self):
+#       data = {
+#           'userID': '69'
+#       }
+#       r = requests.post(URL_ROOT + '/commit_buy', json=data)
 
 
-def test_commit_sell():
-    data = {
-        'userID': '69'
-    }
-
-    r = requests.post(URL_ROOT + '/commit_sell', json=data)
-
-def test_cancel_sell():
-    data = {
-        'userID': '69'
-    }
-
-    r = requests.post(URL_ROOT + '/cancel_sell', json=data)
+#   def test_cancel_buy(self):
+#       data = {
+#           'userID': '69'
+#       }
+#       r = requests.post(URL_ROOT + '/cancel_buy', json=data)
 
 
-def test_set_buy_amount():
-    data = {
-        'userID': '69',
-        'amount': 200,
-        'symbol': 'abc'
-    }
-
-    r = requests.post(URL_ROOT + '/set_buy_amount', json=data)
-
-
-def test_set_sell_amount():
-    data = {
-        'userID': '69',
-        'amount': 200,
-        'symbol': 'abc'
-    }
-
-    r = requests.post(URL_ROOT + '/set_sell_amount', json=data)
+#   def test_sell(self):
+#       data = {
+#           'userID': '69',
+#           'amount': 300,
+#           'symbol': 'abc'
+#       }
+#       r = requests.post(URL_ROOT + '/sell', json=data)
 
 
-def test_cancel_set_buy():
-    data = {
-        'userID': '69',
-        'symbol': 'abc'
-    }
-
-    r = requests.post(URL_ROOT + '/cancel_set_buy', json=data)
+#   def test_commit_sell(self):
+#       data = {
+#           'userID': '69'
+#       }
+#       r = requests.post(URL_ROOT + '/commit_sell', json=data)
 
 
-def test_set_buy_trigger():
-    data = {
-        'userID': '69',
-        'symbol': 'abc',
-        'price': 69
-    }
-
-    r = requests.post(URL_ROOT + '/set_buy_trigger', json=data)
+#   def test_cancel_sell(self):
+#       data = {
+#           'userID': '69'
+#       }
+#       r = requests.post(URL_ROOT + '/cancel_sell', json=data)
 
 
-def test_set_sell_trigger():
-    data = {
-        'userID': '69',
-        'symbol': 'abc',
-        'price': 69
-    }
-
-    r = requests.post(URL_ROOT + '/set_sell_trigger', json=data)
-
-
-def test_cancel_set_sell():
-    data = {
-        'userID': '69',
-        'symbol': 'abc'
-    }
-
-    r = requests.post(URL_ROOT + '/cancel_set_sell', json=data)
+#   def test_set_buy_amount(self):
+#       data = {
+#           'userID': '69',
+#           'amount': 200,
+#           'symbol': 'abc'
+#       }
+#       r = requests.post(URL_ROOT + '/set_buy_amount', json=data)
 
 
-def test_user_dumplog():
-    data = {
-        'userID': '69',
-        'filename': 'user_dumplog'
-    }
-
-    r = requests.post(URL_ROOT + '/dumplog', json=data)
-
-
-def test_system_dumplog():
-    data = {
-        'filename': 'system_dumplog',
-    }
-
-    r = requests.post(URL_ROOT + '/dumplog', json=data)
+#   def test_set_sell_amount(self):
+#       data = {
+#           'userID': '69',
+#           'amount': 200,
+#           'symbol': 'abc'
+#       }
+#       r = requests.post(URL_ROOT + '/set_sell_amount', json=data)
 
 
-def test_display_summary():
-    data = {
-        'userID': '69'
-    }
+#   def test_cancel_set_buy(self):
+#       data = {
+#           'userID': '69',
+#           'symbol': 'abc'
+#       }
+#       r = requests.post(URL_ROOT + '/cancel_set_buy', json=data)
 
-    r = requests.post(URL_ROOT + '/display_summary', json=data)
+
+#   def test_set_buy_trigger(self):
+#       data = {
+#           'userID': '69',
+#           'symbol': 'abc',
+#           'price': 69
+#       }
+#       r = requests.post(URL_ROOT + '/set_buy_trigger', json=data)
 
 
-def test_quote_handler():
-    data = {
-        'userID': '69',
-        'symbol': 'abc'
-    }
-    r = requests.post(URL_ROOT + '/quote', json=data)
+#   def test_set_sell_trigger(self):
+#       data = {
+#           'userID': '69',
+#           'symbol': 'abc',
+#           'price': 69
+#       }
+#       r = requests.post(URL_ROOT + '/set_sell_trigger', json=data)
+
+
+#   def test_cancel_set_sell(self):
+#       data = {
+#           'userID': '69',
+#           'symbol': 'abc'
+#       }
+#       r = requests.post(URL_ROOT + '/cancel_set_sell', json=data)
+
+
+#   def test_user_dumplog(self):
+#       data = {
+#           'userID': '69',
+#           'filename': 'user_dumplog'
+#       }
+#       r = requests.post(URL_ROOT + '/dumplog', json=data)
+
+
+#   def test_system_dumplog(self):
+#       data = {
+#           'filename': 'system_dumplog',
+#       }
+#       r = requests.post(URL_ROOT + '/dumplog', json=data)
+
+
+#   def test_display_summary(self):
+#       data = {
+#           'userID': '69'
+#       }
+#       r = requests.post(URL_ROOT + '/display_summary', json=data)
+
+
+#   def test_quote_handler(self):
+#       data = {
+#           'userID': '69',
+#           'symbol': 'abc'
+#       }
+#       r = requests.post(URL_ROOT + '/quote', json=data)
+
+    def tearDown(self):
+        self.c.execute('DELETE FROM USERS;')
 
 
 if __name__ == '__main__':
-
-    # test_add()
-    # test_buy()
-    # test_commit_buy()
-    # test_sell()
-    # test_commit_sell()
-    # test_cancel_sell()
-    # test_cancel_buy()
-    # test_set_buy_amount()
-    # test_set_sell_amount()
-    # test_cancel_set_buy()
-    # test_set_buy_trigger()
-    # test_set_sell_trigger()
-    # test_cancel_set_sell()
-    # test_user_dumplog()
-    # test_system_dumplog()
-    # test_quote_handler()
-    test_quote_handler()
+  unittest.main()
