9a49b7e91a4bb975384e786e818dcba313f0b5bd
osboxes.org <shreyasdevalapurkar@gmail.com>
2019-04-02 20:42:42 -0400

    get all the necessary user summary info from backend

--- transaction-server/src/client.go
+++ transaction-server/src/client.go
@@ -19,10 +19,18 @@ func getUserDataHandler(w http.ResponseWriter, r *http.Request) {
 	response := struct {
 		Balance      float64
 		StockAmounts []StockRows
-	}{0, nil}
+		BuyTriggers []BuyTriggerRows
+		SellTriggers []SellTriggerRows
+		BuyAmounts []BuyAmountRows
+		SellAmounts []SellAmountRows
+	}{0, nil, nil, nil, nil, nil}
 
 	response.Balance = getBalance(req.UserID)
 	response.StockAmounts = getOwnedStocks(req.UserID)
+	response.BuyTriggers = getSetBuyTriggers(req.UserID)
+	response.SellTriggers = getSetSellTriggers(req.UserID)
+	response.BuyAmounts = getSetBuyAmounts(req.UserID)
+	response.SellAmounts = getSetSellAmounts(req.UserID)
 
 	payload, _ := json.Marshal(response)
 
@@ -88,13 +96,110 @@ func getOwnedStocks(UserID string) []StockRows {
 	return stocks
 }
 
-// type BuyTriggerRows struct {
-// 	Symbol string
-// 	Amount float64
-// }
+type BuyTriggerRows struct {
+	Symbol string
+	Price float64
+}
 
-// func getSetBuyTriggers(UserID string) []BuyTriggerRows {
-// 	buyTriggers := []BuyTriggerRows{}
+func getSetBuyTriggers(UserID string) []BuyTriggerRows {
+	buyTriggers := []BuyTriggerRows{}
 
-// 	queryString := "SELECT symbol, price FROM triggers"
-// }
+	queryString := "SELECT symbol, price FROM triggers WHERE user_id = $1 AND method = 'buy';"
+	rows, err := db.Query(queryString, UserID)
+
+	defer rows.Close()
+
+	if err != nil {
+		failGracefully(err, "Failed to select buy triggers")
+	}
+
+	for rows.Next() {
+		buyTrigger := BuyTriggerRows{}
+		if err := rows.Scan(&buyTrigger.Symbol, &buyTrigger.Price); err != nil {
+			failGracefully(err, "Failed to parse symbol and price")
+		}
+		buyTriggers = append(buyTriggers, buyTrigger)
+	}
+	return buyTriggers
+}
+
+type SellTriggerRows struct {
+	Symbol string
+	Price float64
+}
+
+func getSetSellTriggers(UserID string) []SellTriggerRows {
+	sellTriggers := []SellTriggerRows{}
+
+	queryString := "SELECT symbol, price FROM triggers WHERE user_id = $1 AND method = 'sell';"
+	rows, err := db.Query(queryString, UserID)
+
+	defer rows.Close()
+
+	if err != nil {
+		failGracefully(err, "Failed to select sell triggers")
+	}
+
+	for rows.Next() {
+		sellTrigger := SellTriggerRows{}
+		if err := rows.Scan(&sellTrigger.Symbol, &sellTrigger.Price); err != nil {
+			failGracefully(err, "Failed to parse symbol and price")
+		}
+		sellTriggers = append(sellTriggers, sellTrigger)
+	}
+	return sellTriggers
+}
+
+type BuyAmountRows struct {
+	Symbol string
+	Quantity float64
+}
+
+func getSetBuyAmounts(UserID string) []BuyAmountRows {
+	buyAmounts := []BuyAmountRows{}
+
+	queryString := "SELECT symbol, quantity FROM buy_amounts WHERE user_id = $1;"
+	rows, err := db.Query(queryString, UserID)
+
+	defer rows.Close()
+
+	if err != nil {
+		failGracefully(err, "Failed to select buy amounts")
+	}
+
+	for rows.Next() {
+		buyAmount := BuyAmountRows{}
+		if err := rows.Scan(&buyAmount.Symbol, &buyAmount.Quantity); err != nil {
+			failGracefully(err, "Failed to parse symbol and quantity")
+		}
+		buyAmounts = append(buyAmounts, buyAmount)
+	}
+	return buyAmounts
+}
+
+type SellAmountRows struct {
+	Symbol string
+	Quantity float64
+}
+
+func getSetSellAmounts(UserID string) []SellAmountRows {
+	sellAmounts := []SellAmountRows{}
+
+	queryString := "SELECT symbol, quantity FROM sell_amounts WHERE user_id = $1;"
+	rows, err := db.Query(queryString, UserID)
+
+	defer rows.Close()
+
+	if err != nil {
+		failGracefully(err, "Failed to select sell amounts")
+	}
+
+	for rows.Next() {
+		sellAmount := SellAmountRows{}
+		if err := rows.Scan(&sellAmount.Symbol, &sellAmount.Quantity); err != nil {
+			failGracefully(err, "Failed to parse symbol and quantity")
+		}
+		sellAmounts = append(sellAmounts, sellAmount)
+	}
+	return sellAmounts
+}
