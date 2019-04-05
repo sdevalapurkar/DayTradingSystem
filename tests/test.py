6b092c8bc4463fdd9ecc1eafe11a600eddce2d72
Caity Gossland <caitygossland@gmail.com>
2019-04-05 16:30:23 -0400

    adds buy, starts sell

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,7 @@
 import unittest
 import requests
 import time
+import re
 from crate import client
 
 URL_ROOT = 'http://localhost:8080'
@@ -10,72 +11,94 @@ class TestTransactionServer(unittest.TestCase):
     def setUp(self):
         self.connection = client.connect("http://localhost:4200", username="crate")
         self.c = self.connection.cursor()
-
-    def test_add(self):
+        self.symbol = 'abc'
+        self.user = 'User1'
         data = {
-          'userID': 'User1',
-          'amount': 100.00,
+            'userID': self.user,
+            'symbol': self.symbol
+        }
+        r = requests.post(URL_ROOT + '/quote', json=data)
+        self.quote = round(float(re.search(r'.*?,.*?,(.*?)$', r.text).group(1)), 2)
+        print('quote is: ', self.quote)
+        
+
+    def test_0_add(self):
+        data = {
+          'userID': self.user,
+          'amount': self.quote*2,
           'transactionNum': 1
         }
         requests.post(URL_ROOT + '/add', json=data)
         time.sleep(1)
         c = self.connection.cursor()
-        c.execute('SELECT * FROM users;')
-        self.assertEqual(c.fetchone(), [100.0, 'User1'])
+        c.execute("SELECT * FROM users WHERE user_id=?;", (self.user,))
+        self.assertEqual(c.fetchone(), [self.quote*2, self.user])
 
-    def test_buy(self):
+    def test_1_buy(self):
         data = {
-          'userID': 'User1',
-          'amount': 100.00,
-          'symbol': 'abc',
+          'userID': self.user,
+          'amount': self.quote,
+          'symbol': self.symbol,
           'transactionNum': 2
         }
         requests.post(URL_ROOT + '/buy', json=data)
-        time.sleep(1)
+        time.sleep(2)
         c = self.connection.cursor()
-        c.execute('SELECT * FROM users;')
-        self.assertEqual(c.fetchone(), [100.0, 'User1'])
+        c.execute("SELECT * FROM users WHERE user_id=?;", (self.user,))
+        self.assertEqual(c.fetchone(), [self.quote, self.user])
 
 
-    def test_commit_buy(self):
+    def test_2_commit_buy(self):
         data = {
-            'userID': 'User1',
+            'userID': self.user,
             'transactionNum': 3
         }
         requests.post(URL_ROOT + '/commit_buy', json=data)
         time.sleep(1)
         c = self.connection.cursor()
-        c.execute('SELECT * FROM users;')
-        self.assertEqual(c.fetchone(), [0.0, 'User1'])
+        c.execute("SELECT * FROM stocks WHERE user_id=?;", (self.user,))
+        self.assertEqual(c.fetchone(), [1, self.symbol, self.user])
 
-#   def test_cancel_buy(self):
-#       data = {
-#           'userID': '69'
-#       }
-#       r = requests.post(URL_ROOT + '/cancel_buy', json=data)
-
-
-#   def test_sell(self):
-#       data = {
-#           'userID': '69',
-#           'amount': 300,
-#           'symbol': 'abc'
-#       }
-#       r = requests.post(URL_ROOT + '/sell', json=data)
-
-
-#   def test_commit_sell(self):
-#       data = {
-#           'userID': '69'
-#       }
-#       r = requests.post(URL_ROOT + '/commit_sell', json=data)
+    def test_cancel_buy(self):
+        data = {
+          'userID': self.user,
+          'amount': self.quote,
+          'symbol': self.symbol,
+          'transactionNum': 4
+        }
+        requests.post(URL_ROOT + '/buy', json=data)
+        time.sleep(1)
+        r = requests.post(URL_ROOT + '/cancel_buy', json=data)
+        c = self.connection.cursor()
+        c.execute("SELECT * FROM stocks WHERE user_id=?;", (self.user,))
+        self.assertEqual(c.fetchone(), [1, self.symbol, self.user])
 
 
-#   def test_cancel_sell(self):
-#       data = {
-#           'userID': '69'
-#       }
-#       r = requests.post(URL_ROOT + '/cancel_sell', json=data)
+    def test_sell(self):
+        data = {
+            'userID': self.user,
+            'amount': self.quote,
+            'symbol': self.symbol
+        }
+        r = requests.post(URL_ROOT + '/sell', json=data)
+
+
+    def test_commit_sell(self):
+        data = {
+            'userID': self.user
+        }
+        r = requests.post(URL_ROOT + '/commit_sell', json=data)
+
+
+    def test_cancel_sell(self):
+        data = {
+            'userID': self.user,
+            'amount': self.quote,
+            'symbol': self.symbol
+        }
+        r = requests.post(URL_ROOT + '/sell', json=data)
+        time.sleep(1)
+        r = requests.post(URL_ROOT + '/cancel_sell', json=data)
 
 
 #   def test_set_buy_amount(self):
@@ -152,17 +175,10 @@ class TestTransactionServer(unittest.TestCase):
 #       r = requests.post(URL_ROOT + '/display_summary', json=data)
 
 
-#   def test_quote_handler(self):
-#       data = {
-#           'userID': '69',
-#           'symbol': 'abc'
-#       }
-#       r = requests.post(URL_ROOT + '/quote', json=data)
-
-    # def tearDown(self):
-    #     self.c.execute('DELETE FROM users;')
-    #     self.c.execute('DELETE FROM buy_amounts;')
-    #     self.connection.close()
+#    def tearDownClass(self):
+#        self.c.execute('DELETE FROM users;')
+#        self.c.execute('DELETE FROM stocks;')
+#        self.connection.close()
 
 if __name__ == '__main__':
   unittest.main(warnings='ignore')
